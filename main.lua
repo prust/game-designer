cfe2689532616604fad9687d2d9d063190116f37
Peter Rust <peter@cornerstonenw.com>
2017-04-14 22:31:00 -0700

    Cleaned up overly-verbose reporting

--- main.lua
+++ main.lua
@@ -1,19 +1,17 @@
 local async = require('async')
 
 local base_url = 'http://www.unashamedstudio.com/game-designer/'
-local update_status = 'Loading'
+local update_status = ''
 
 function love.load(args)
   if args.updated then
-    update_status = 'load complete'
+    update_status = 'Update complete'
     return
   end
 
-  update_status = 'loading async'
   async.load()
   async.ensure.atLeast(2)
   
-  update_status = 'defining async requests'
   local version = nil
   local version_request = async.define("version_request", function()
     local http = require("socket.http")
@@ -27,18 +25,15 @@ function love.load(args)
     return http.request(base_url .. 'game_' .. version .. '.love')
   end)
   
-  update_status = 'making request';
+  update_status = 'Checking for updates...';
   version_request(function(result, status)
-    update_status = 'response: ' .. status
     if status == 200 then
-      update_status = 'getting version meta'
       local getVersionMeta = assert(loadstring("return " .. result))
       version = getVersionMeta().version
       if not love.filesystem.isFile('game_' .. version .. '.love') then
-        update_status = 'downloading game'
+        update_status = 'Update found, downloading...'
         game_request(function(result, status)
           if status == 200 then
-            update_status = 'writing downloaded game & loading'
             love.filesystem.write('game_' .. version .. '.love', result)  
             love.filesystem.mount('game_' .. version .. '.love', '')
             package.loaded.main = nil
@@ -49,7 +44,7 @@ function love.load(args)
           end
         end, version)
       else
-        update_status = 'loading game from disk'
+        update_status = 'No new updates found, loading game...'
         love.filesystem.mount('game_' .. version .. '.love')
         package.loaded.main = nil
         package.loaded.conf = nil
